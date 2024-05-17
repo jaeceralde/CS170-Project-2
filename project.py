31a0431d5f8b59c3646b26310e526d53b7c213b2
jaeceralde <jaeceralde@yahoo.com>
2024-05-17 00:34:15 -0700

    started the node class

--- project.py
+++ project.py
@@ -1,5 +1,10 @@
 import random
-
+import numpy as np
 
 def getRand():
     return random.random()
+
+class node:
+    accuracy = 0 # keeps track of accuracy score
+    remainingFeatures = np.array(0) # keeps track of unvisited states(?)
+    featuresSubset = {} # keeps track of visited states(?)
