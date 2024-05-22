1e04a2b8127f4acab11fb59c3e7c3664e5c6cee8
bsule <bilal.suleiman@gmail.com>
2024-05-21 20:29:20 -0700

    start code

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import project
 from project import *
 
-print("hello world")
+
+small_data = load('small-test-dataset.txt')
+large_data = load('large-test-dataset.txt')
+
+
+
+small_feat = [3, 5, 7]
+small_acc = 0
+print(f'Accuracy on small dataset: {small_acc:.2f}')
+
+large_feat = [1, 15, 27]
+large_acc = 0
+print(f'Accuracy on large dataset: {large_acc:.2f}')
--- project.py
+++ project.py
@@ -1,2 +1,34 @@
 import numpy as np
 
+
+class Classifier:
+    def __init__(self):
+        self.train_data = None # list of the training data
+        self.selected_features = None # the features that are inputted
+        
+    def train(self, data, selected_features):
+        self.train_data = data
+        self.selected_features = selected_features
+        
+    def euclidean_distance(self, p1, p2):
+        
+    
+    def test(self, instance):
+        # here we go through train_data and find which is the closest point
+        
+    
+
+class Validator:
+    def leave_one_out_validation(self, classifier, data, selected_features):
+        correct_pred = 0
+        total = len(data)
+        # go through all the data and run train and test and count how many
+        # are right then return correct_pred / total
+        
+        
+def load(filename):
+    data = np.loadtxt(filename)
+
+    # figure out what to do with data here
+    
+    return data
