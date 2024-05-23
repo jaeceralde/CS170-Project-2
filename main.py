83f0c2545f95a472bc2820ff83da7591dc9325ae
jaeceralde <jaeceralde@yahoo.com>
2024-05-23 00:30:19 -0700

    minor fixes in formatting && added some trace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import project
-from project import Classifier, Validator, load
+from project import *
 
 # load datasets
 small_data = load('small-test-dataset.txt')
@@ -10,14 +10,12 @@ classifier = Classifier()
 validator = Validator()
 
 
-small_feat = [3, 5, 7]
-#small_acc = 0
 # calculated the accuracy of the nearest neighbor classifier on the small dataset
-small_acc = validator.leave_one_out_validation(classifier, small_data, small_feat)
+small_features = [3, 5, 7]
+small_acc = validator.leave_one_out_validation(classifier, small_data, small_features)
 print(f'Accuracy on small dataset: {small_acc:.2f}')
 
-large_feat = [1, 15, 27]
-#large_acc = 0
 # calculated the accuracy of the nearest neighbor classifier on the large dataset
-large_acc = validator.leave_one_out_validation(classifier, large_data, large_feat)
+large_features = [1, 15, 27]
+large_acc = validator.leave_one_out_validation(classifier, large_data, large_features)
 print(f'Accuracy on large dataset: {large_acc:.2f}')
--- project.py
+++ project.py
@@ -3,57 +3,59 @@ import numpy as np
 
 class Classifier:
     def __init__(self):
-        self.train_data = None # list of the training data
-        self.selected_features = None # the features that are inputted
+        self.train_data = None  # list of the training data
+        self.selected_features = None   # the features that are inputted
         
     def train(self, data, selected_features):
         self.train_data = data
         self.selected_features = selected_features
         
     def euclidean_distance(self, p1, p2):
-        return np.sqrt(np.sum((p1[self.selected_features] - p2[self.selected_features]) ** 2)) # calculate euclidean distance 
+        return np.sqrt(np.sum((p1[self.selected_features] - p2[self.selected_features]) ** 2))  # calculate euclidean distance 
     
+    # here we go through train_data and find which is the closest point
     def test(self, instance):
-        # here we go through train_data and find which is the closest point
-        
         # find the nearest neighbor 
         # compute the distance between a given test instance and all training instances in the dataset
         distance = [self.euclidean_distance(instance, train_instance) for train_instance in self.train_data]
-        nn_index = np.argmin(distance) # get the index from the smallest distance
+        nn_index = np.argmin(distance)  # get the index from the smallest distance
         return self.train_data[nn_index, 0] # return the class label of the nearest neighbor
 
-        
-    
 
 class Validator:
     def leave_one_out_validation(self, classifier, data, selected_features):
         correct_pred = 0
         total = len(data)
-        # go through all the data and run train and test and count how many
-        # are right then return correct_pred / total
-
+        
+        # go through all the data and run train and test
+        # count how many are right then return correct_pred / total
         for i in range(total):
             # remove the test instance from the original dataset
             train_data = np.delete(data, i, axis = 0)
             # select the ith instance from the original dataset to be used as the test instance 
             test_instance = data[i]
 
+            print(f'The test instance\'s actual label is:', test_instance[0])   # for trace
+
             # train the classifier
             classifier.train(train_data, selected_features)
             # test the classifier
             predictor = classifier.test(test_instance)
 
-            #if the predictor to the actual label
+            print(f'The predictor says the test instance\'s label is:', predictor)  # for trace
+
+            # if the predictor is the actual label
             if predictor == test_instance[0]:
-                correct_pred += 1  
+                correct_pred += 1
+                print('\tRIGHT PREDICTION\n')   # for trace
+            else:
+                print('\tWRONG PREDICTION\n')   # for trace
 
         return correct_pred / total
         
         
 def load(filename):
     data = np.loadtxt(filename)
-
-    # figure out what to do with data here
 
     # seperate labels and features 
     labels = data[:, 0]
