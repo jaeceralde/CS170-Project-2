a1e73b1c8f6e7fcb051edd7459b395377c646704
jaeceralde <jaeceralde@yahoo.com>
2024-05-23 19:36:18 -0700

    added print for features list (trace purposes)

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ validator = Validator()
 
 # calculated the accuracy of the nearest neighbor classifier on the small dataset
 small_features = [3, 5, 7]
+print('Using feature(s): ' + ''.join(str(small_features)) + '\n')  # print out small_features for trace
 small_acc = validator.leave_one_out_validation(classifier, small_data, small_features)
 print(f'Accuracy on small dataset: {small_acc:.2f}\n')  # newline for trace format
 
 # calculated the accuracy of the nearest neighbor classifier on the large dataset
 large_features = [1, 15, 27]
+print('Using feature(s): ' + ''.join(str(large_features)) + '\n')  # print out large_features for trace
 large_acc = validator.leave_one_out_validation(classifier, large_data, large_features)
 print(f'Accuracy on large dataset: {large_acc:.2f}')
